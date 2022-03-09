8441b9abdbeaaef2fd205fdf4a0dafc9a9f5e83a
MD. MAHADI HASAN <83344521+mahadidev7@users.noreply.github.com>
2022-03-10 05:04:03 +0600

    preloader off
    preloader off

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,11 +5,9 @@
 	// Window Resize Mobile Menu Fix
 	mobileNav();
 
-
 	// Scroll animation init
 	window.sr = new scrollReveal();
 	
-
 	// Menu Dropdown Toggle
 	if($('.menu-trigger').length){
 		$(".menu-trigger").on('click', function() {	
@@ -17,7 +15,6 @@
 			$('.header-area .nav').slideToggle(200);
 		});
 	}
-
 
 	// Menu elevator animation
 	$('a[href*=\\#]:not([href=\\#])').on('click', function() {
@@ -95,22 +92,22 @@
 
 
 	// Page loading animation
-	$(window).on('load', function() {
-		if($('.cover').length){
-			$('.cover').parallax({
-				imageSrc: $('.cover').data('image'),
-				zIndex: '1'
-			});
-		}
+// 	$(window).on('load', function() {
+// 		if($('.cover').length){
+// 			$('.cover').parallax({
+// 				imageSrc: $('.cover').data('image'),
+// 				zIndex: '1'
+// 			});
+// 		}
 
-		$("#preloader").animate({
-			'opacity': '0'
-		}, 600, function(){
-			setTimeout(function(){
-				$("#preloader").css("visibility", "hidden").fadeOut();
-			}, 300);
-		});
-	});
+// 		$("#preloader").animate({
+// 			'opacity': '0'
+// 		}, 600, function(){
+// 			setTimeout(function(){
+// 				$("#preloader").css("visibility", "hidden").fadeOut();
+// 			}, 300);
+// 		});
+// 	});
 
 
 	// Window Resize Mobile Menu Fix
